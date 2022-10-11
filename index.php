2b819d849b5151c17c2662223856d85229a89028
Thomas Aldaitz <thomas.aldaitz@wildcodeschool.com>
2022-10-11 12:43:33 +0200

    Correction of workshop

--- index.php
+++ index.php
@@ -2,3 +2,33 @@
 
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+require_once 'src/Fighter.php';
+
+$heracles = new Fighter("Heracles", 20, 6);
+$lion = new Fighter("Lion de némée", 11, 13);
+
+
+echo "Combattant 1 : " . $heracles->getName() . ' - ' . $heracles->getLife() . "\n";
+echo "Combattant 2 : " . $lion->getName() . ' - ' . $lion->getLife() . "\n";
+
+$round = 1;
+while(true) {
+    $heracles->fight($lion);
+    if(!$lion->isAlive())
+        break;
+
+    $lion->fight($heracles);
+    if(!$heracles->isAlive())
+        break;
+
+    echo "Round : $round\n";
+    echo "Combattant 1 : " . $heracles->getName() . ' - ' . $heracles->getLife() . "\n";
+    echo "Combattant 2 : " . $lion->getName() . ' - ' . $lion->getLife() . "\n";
+
+    $round++;
+}
+
+echo "*******************************************\n";
+echo "Combattant 1 : " . $heracles->getName() . ' - ' . $heracles->getLife() . "\n";
+echo "Combattant 2 : " . $lion->getName() . ' - ' . $lion->getLife() . "\n";
--- src/Fighter.php
+++ src/Fighter.php
@@ -1,3 +1,64 @@
 <?php
 
 /* Fighter class definition */
+
+class Fighter 
+{
+    public const MAX_LIFE = 100;
+
+    private string $name;
+    private int $strength;
+    private int $dexterity;
+    private int $life = self::MAX_LIFE;
+
+    public function __construct(string $name, int $strength, int $dexterity)
+    {
+        $this->name = $name;
+        $this->strength = $strength;
+        $this->dexterity = $dexterity;
+    }
+
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    public function getLife() : int
+    {
+        return $this->life;
+    }
+
+    public function takeDamage(int $damage) : void
+    {
+        if($damage > $this->life) {
+            $this->life = 0;
+        } else {
+            $this->life -= $damage;
+        }
+    }
+
+    public function isAlive() : bool
+    {
+        if($this->life == 0)
+            return false;
+            
+        return true;
+    }
+
+    public function getDexterity() : int
+    {
+        return $this->dexterity;
+    }
+
+    public function fight(Fighter $opponent) : void
+    {
+        $attack = rand(1, $this->strength);
+        $defense = $opponent->getDexterity();
+
+        $damage = $attack - $defense;
+        $damage = $damage > 0 ? $damage : 0;
+
+        $opponent->takeDamage($damage);
+
+    }
+}
